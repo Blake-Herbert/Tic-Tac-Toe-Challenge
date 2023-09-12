c91c49e27eee228171ee13262c4857348bd4987f
Blake Herbert <Bwherb1@gmail.com>
2023-09-12 18:58:00 -0400

    condensed claimSpace functions

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 const b1 = document.getElementById("b1");
-b1.addEventListener("click", TakeMove);
+b1.addEventListener("click", ClaimSpace);
 const b2 = document.getElementById("b2");
-b2.addEventListener("click", TakeMove);
+b2.addEventListener("click", ClaimSpace);
 const b3 = document.getElementById("b3");
-b3.addEventListener("click", TakeMove);
+b3.addEventListener("click", ClaimSpace);
 const b4 = document.getElementById("b4");
-b4.addEventListener("click", TakeMove);
+b4.addEventListener("click", ClaimSpace);
 const b5 = document.getElementById("b5");
-b5.addEventListener("click", TakeMove);
+b5.addEventListener("click", ClaimSpace);
 const b6 = document.getElementById("b6");
-b6.addEventListener("click", TakeMove);
+b6.addEventListener("click", ClaimSpace);
 const b7 = document.getElementById("b7");
-b7.addEventListener("click", TakeMove);
+b7.addEventListener("click", ClaimSpace);
 const b8 = document.getElementById("b8");
-b8.addEventListener("click", TakeMove);
+b8.addEventListener("click", ClaimSpace);
 const b9 = document.getElementById("b9");
-b9.addEventListener("click", TakeMove);
+b9.addEventListener("click", ClaimSpace);
 
 let currentPlayer = "x";
 
-function TakeMove()
-{ if(document.getElementById("b1").innerText != "x" && document.getElementById("b1").innerText != "o") 
-{
-    document.getElementById("b1").innerText = currentPlayer;
-    CheckWinCondition();
-    
-    if(currentPlayer == "x") 
-    { currentPlayer = "o";
-    } else currentPlayer ="x"
+////////////////////////////////
 
-} else console.log("invalid move");
+function ClaimSpace(Event, SelectedButton) {
+    console.log(Event.target.id);
+    if (Event.target.id.innerText != "x" && Event.target.id.innerText != "o") {
+        Event.target.innerText = currentPlayer;
+        CheckWinCondition;
+
+        if (currentPlayer == "x") {
+            currentPlayer = "o";
+        } else currentPlayer = "x"
+
+    } else console.log("invalid move");
 }
 
 
-function CheckWinCondition()
-{
-    if( currentPlayer == b1.innerText == b2.innerText == b3.innerText ||
-        currentPlayer == b1.innerText == b5.innerText == b9.innerText ||
-        currentPlayer == b1.innerText == b4.innerText == b7.innerText ||
-        currentPlayer == b2.innerText == b5.innerText == b8.innerText ||
-        currentPlayer == b3.innerText == b5.innerText == b7.innerText ||
-        currentPlayer == b3.innerText == b6.innerText == b9.innerText ||
-        currentPlayer == b4.innerText == b5.innerText == b6.innerText ||
-        currentPlayer == b7.innerText == b8.innerText == b9.innerText)
-        alert(currentPlayer + " wins!");
+function CheckWinCondition() {
+    if (document.getElementById("b1").innerText = document.getElementById("b2").innerText
+        // currentPlayer == document.getElementById("b1").innerText == document.getElementById("b2").innerText == document.getElementById("b3").innerText ||
+        // currentPlayer == document.getElementById("b1").innerText == document.getElementById("b5").innerText == document.getElementById("b9").innerText ||
+        // currentPlayer == document.getElementById("b1").innerText == document.getElementById("b4").innerText == document.getElementById("b7").innerText ||
+        // currentPlayer == document.getElementById("b2").innerText == document.getElementById("b5").innerText == document.getElementById("b8").innerText ||
+        // currentPlayer == document.getElementById("b3").innerText == document.getElementById("b5").innerText == document.getElementById("b7").innerText ||
+        // currentPlayer == document.getElementById("b3").innerText == document.getElementById("b6").innerText == document.getElementById("b9").innerText ||
+        // currentPlayer == document.getElementById("b4").innerText == document.getElementById("b5").innerText == document.getElementById("b6").innerText ||
+        // currentPlayer == document.getElementById("b7").innerText == document.getElementById("b8").innerText == document.getElementById("b9").innerText
+    ) {
+        console.log("win");
+    }
 }
